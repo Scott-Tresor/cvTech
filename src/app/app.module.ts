20afbd9c27d34b46dc04299966d993df35302160
scott-Tresor <scotttresor@gmail.com>
2020-04-11 14:03:41 +0200

    feat: create a new project on angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ColorComponent } from './color/color.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -19,8 +15,6 @@ import { FormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [
     AppComponent,
-    ColorComponent,
-    NavbarComponent
   ]
 })
 export class AppModule { }

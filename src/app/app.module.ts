227981bb0650807d3fc0a039ed0c1f1f5b4577ae
scott-Tresor <scotttresor@gmail.com>
2020-04-11 14:13:43 +0200

    feat: add differents component in ours project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ItemCvComponent } from './CvTech/item-cv/item-cv.component';
+import { DetailCvComponent } from './CvTech/detail-cv/detail-cv.component';
+import { ListeCvComponent } from './CvTech/liste-cv/liste-cv.component';
+import { CvComponent } from './CvTech/cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ItemCvComponent,
+    DetailCvComponent,
+    ListeCvComponent,
+    CvComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,6 +23,10 @@ import { FormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [
     AppComponent,
+    ItemCvComponent,
+    DetailCvComponent,
+    ListeCvComponent,
+    CvComponent,
   ]
 })
 export class AppModule { }

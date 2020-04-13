46317b04bd33a4e274e7d78e411685c22e82f479
scott-Tresor <scotttresor@gmail.com>
2020-04-13 16:39:25 +0200

    review: review code because syntax error

--- src/app/CvTech/cv/cv.component.ts
+++ src/app/CvTech/cv/cv.component.ts
@@ -2,30 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Personne } from '../../model/personne';
 
 @Component({
-  selector: 'app-cv',
-  templateUrl: './cv.component.html',
-  styleUrls: []
+    selector: 'app-cv',
+    templateUrl: './cv.component.html',
+    styleUrls: []
 })
 export class CvComponent implements OnInit {
 
-  personnes: Personne [];
-  selectedPersonne: Personne;
+    personnes: Personne[];
 
-  constructor() { }
+    selectedPersonne: Personne;
 
-  user(){
-    this.personnes = [
-        new  Personne(1,'scott','tresor',25,'cott.jpg','developpeur',77777),
-        new Personne(2,'ilunga','kasenda',26,'ilunga.jpg','ingenieur',34567),
-    ];
-  }
+    constructor() {}
 
-  selectPersonne(personne)
-  {
-    this.selectedPersonne = personne;
-  }
+    selectPersonne(personne) {
+        this.selectedPersonne = personne;
+    }
 
-  ngOnInit() {
-    this.user();
-  }
+    ngOnInit() {
+        this.personnes = [
+            new Personne(1, 'scott', 'tresor', 25, 'cott.jpg', 'developpeur', 77777),
+            new Personne(2, 'scott', 'tresor', 24, 'Sott.jpg', 'developpeur', 77778),
+            new Personne(3, 'scott', 'tresor', 23, 'trott.jpg', 'developpeur', 77779),
+            new Personne(4, 'scott', 'tresor', 22, 'vott.jpg', 'developpeur', 77780)
+        ];
+    }
 }

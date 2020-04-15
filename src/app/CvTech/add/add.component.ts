ceb09634459a030d296c9d3ebbc277bebddff1aa
scott-Tresor <scotttresor@gmail.com>
2020-04-15 17:49:03 +0200

    feat:review form add new users

--- src/app/CvTech/add/add.component.ts
+++ src/app/CvTech/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Personne } from '../../model/personne';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
+    personne: Personne[];
+    name: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addPersonne()
+  {
+    console.log({name: this.name});
+  }
 }
